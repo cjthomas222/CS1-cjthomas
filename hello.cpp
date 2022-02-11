d61218ab4bda724f89a5ebc485649166143f61ef
cjthomas <cjthomas@mavs.coloradomesa.edu>
2022-02-11 11:33:29 -0700

    ascii art code output

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,20 @@
+/*
+This is a multi-line comment
+cool huh?
+This is the first Hello World Program
+*/
+
+
 #include <iostream>
 
 using namespace std;
 
-int main() {
+int main(){
+    /*
+    This is the main function for this program
+    */
+    //This is a single line comment
+    int x;
     cout << "Hello World" << endl;
     return 0;
 }
---
+++ lecture_notes/datatypes.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+   int number1;
+   int int_num;
+   unsigned int unsign_int;
+
+    
+    number1 = 25;
+
+    cout << "The value of number1 is: " 
+        <<number1 << endl;
+
+    return 0;
+}
