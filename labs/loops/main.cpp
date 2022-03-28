497bfe50d94f274fb9c0c3f05730e977a100c9e8
cjthomas <cjthomas@mavs.coloradomesa.edu>
2022-03-27 19:23:52 -0600

    finished loops lab

--- labs/loops/main.cpp
+++ labs/loops/main.cpp
@@ -6,10 +6,14 @@
 */
 
 #include <iostream>
-#include <iomanip>
+#include <cstdio>
 #include <string>
+#include <cassert>
+#include <iomanip>
 
 using namespace std;
+
+bool program();
 
 
 void printTriangle(int height) {
@@ -28,41 +32,54 @@ void printTriangle(int height) {
 
 
 void printFlippedTriangle(int height) {
-    /* 
-    Implement the function that takes height as an argument
-    and prints a triangle with * of given height.
-    Triangle of height 5, e.g., should look like the following.
-    * * * * *
-    * * * *
-    * * *
-    * *
-    *
+//     /* 
+//     Implement the function that takes height as an argument
+//     and prints a triangle with * of given height.
+//     Triangle of height 5, e.g., should look like the following.
+//     * * * * *
+//     * * * *
+//     * * *
+//     * *
+//     *
     
-    */
-    // FIXME3 ...
-    int height;
-    int i;
-    cout << "Enter a height" << endl;
-    cin >> height;
-
-    for(i=height; i > 0; i--)
-    {
-        cout<< "*" << endl;
+//     */
+//     // FIXME3 ...
+int row = height;
+    // row
+    while (row >= 0) {
+        // column
+        for(int col = 1; col<=row; col++)
+            cout << "* ";
+        row -= 1;
+        cout << endl;
     }
 }
 
 
-/*  
-FIXME4
-Design and implement a function that takes an integer as height and
-prints square of the given height with *.
-Square of height 5, e.g., would look like the following.
-*  *  *  *  *  
-*  *  *  *  *   
-*  *  *  *  *   
-*  *  *  *  *   
-*  *  *  *  *   
-*/
+// /*  
+// FIXME4
+// Design and implement a function that takes an integer as height and
+// prints square of the given height with *.
+// Square of height 5, e.g., would look like the following.
+// *  *  *  *  *  
+// *  *  *  *  *   
+// *  *  *  *  *   
+// *  *  *  *  *   
+// *  *  *  *  *   
+// */
+void printSquare(int height) { 
+    int row = height;
+    // row
+    while (row >= 0) {
+        // column
+        for(int col = height; col>=0;col--)
+            cout << "* ";
+        cout << endl;
+        row--;
+    }
+    
+}
+
 
 // function clears the screen system call
 // NOTE: system call is not a security best pracice!
@@ -75,9 +92,12 @@ void clearScreen() {
     #endif
 }
 
-int main(int argc, char* argv[]) {
     // FIXME5 add a loop to make the program to continue to run until the user wants to quit
-    
+int main(int argc, char* argv[]) {
+    bool keepRunning = false;
+    while(!keepRunning){
+    clearScreen();
+
     // FIXME6 call clearScreen function to clear the screen for each round of the loop
     int height;
     cout << "Program prints geometric shapes of given height with *\n";
@@ -86,17 +106,27 @@ int main(int argc, char* argv[]) {
     // call printTriangle function passing user entered height
     printTriangle(height);
 
+    cout<< endl;
     // FIXME7
     // Call printFlippedTriangle passing proper argument
+    printFlippedTriangle(height);
     // Manually test the function
-
+    cout << endl;
     // FIXME6
+    printSquare(height);
     // Call the function defined in FIXME4 passing proper argument
     // Manually test the function
 
     // FIXME9
-    // prompt user to enter y/Y to continue anything else to quit
-
+    char x;
+    cout << "Enter Y/y to continue running program" << endl;
+    cin >> x;
+    if(x == 'y' || x == 'Y'){
+        keepRunning = false;
+    }else {
+        keepRunning = true;
+    }
+    }
     // FIXME10
     // Use conditional statements to break the loop or continue the loop
 
