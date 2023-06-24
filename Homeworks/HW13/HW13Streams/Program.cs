c8a03d4f26fdcafc7373449dd69d3a19e4113cc3
cjthomas222 <cjthomas@mavs.coloradomesa.edu>
2023-06-24 11:10:01 -0600

    homeworks

--- Homeworks/HW13/HW13Streams/Program.cs
+++ Homeworks/HW13/HW13Streams/Program.cs
@@ -2,16 +2,46 @@
 
 namespace HW13Streams
 {
-    class HW13Streams
+    public class HW13Streams
     {
         static void Main(string[] args)
         {
             string fileName = "test.txt";
-            if(!File.Exists(fileName))
+            string fileOutput = "output.txt";
+            using(StreamReader sr = new StreamReader(fileName))
             {
-                Console.WriteLine("File does not exist");
-                return;
+                string line = sr.ReadToEnd();
+                using(StreamWriter sw = new StreamWriter(fileOutput))
+                {
+                    sw.Write(line);
+                    sw.WriteLine("This is a test");
+                }
             }
+
+
+
+
+
+            // if(!File.Exists(fileName))
+            // {
+            //     Console.WriteLine("File does not exist");
+            //     Environment.Exit(1);
+            // }
+
+            // FileStream fs = File.Open(fileName, FileMode.Open);
+            // byte[] bytes = new byte[fs.Length + 10];
+            // long numbytesToRead = fs.Length;
+            // int numbytesRead = 0;
+
+            // do
+            // {
+            //     int n = fs.Read(bytes, numbytesRead, numbytesRead);
+            //     numbytesRead += n;
+            //     numbytesToRead -= n;
+            // } while(numbytesToRead > 0);
+
+            // Console.WriteLine(System.Text.Encoding.UTF8.GetString(bytes));
+            // fs.Close();
         }
     }
 }
